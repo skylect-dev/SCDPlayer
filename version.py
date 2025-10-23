7ae68e33cfc0423e3bfb7008d37d63d4c5e4dd86
skylect-dev <nickman529@gmail.com>
2025-10-23 10:11:24 -0500

    Enhance file handling in library: add files to cache and visible list based on folder structure; update version to 4.0.0 - 'Lost Illusion'

--- version.py
+++ version.py
@@ -1,5 +1,5 @@
 # SCDToolkit Version Information
-__version__ = "3.3.2 - 'Dark Shard'"
+__version__ = "4.0.0 - 'Lost Illusion'"
 __author__ = "skylect-dev"
-__description__ = "A standalone Windows GUI application for playing SCD music files from games like Kingdom Hearts and Final Fantasy XIV with KH Randomizer integration"
+__description__ = "A standalone Windows GUI application for working with SCD music files from games like Kingdom Hearts and Final Fantasy XIV with KH Randomizer integration"
 __url__ = "https://github.com/skylect-dev/SCDToolkit"
