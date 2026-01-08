f6e3434549de6effd7738d2c1264571977a8387f
skylect-dev <nickman529@gmail.com>
2026-01-07 22:52:23 -0600

    Update version to 4.1.0 - 'Deep Drive' and modify release workflow to read version from version.py

--- version.py
+++ version.py
@@ -1,5 +1,5 @@
 # SCDToolkit Version Information
-__version__ = "4.0.3 - 'Lost Illusion'"
+__version__ = "4.1.0 - 'Deep Drive'"
 __author__ = "skylect-dev"
 __description__ = "A standalone Windows GUI application for working with SCD music files from games like Kingdom Hearts and Final Fantasy XIV with KH Randomizer integration"
 __url__ = "https://github.com/skylect-dev/SCDToolkit"
